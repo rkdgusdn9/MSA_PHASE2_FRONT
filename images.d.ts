fb9e79b81da753d70ca9eb9820c7b1a6e7fd1633
rkdgusdn9 <rkdgusdn9@gmail.com>
2018-11-24 00:48:37 +1300

    Final change - images.d.ts

--- images.d.ts
+++ images.d.ts
@@ -5,8 +5,3 @@ declare module '*.jpeg'
 declare module '*.gif'
 declare module '*.bmp'
 declare module '*.tiff'
-<<<<<<< HEAD
-
-=======
-declare module 'msr'
->>>>>>> c373861bdde60a146637ebf3183eb0bbb95d05dc
